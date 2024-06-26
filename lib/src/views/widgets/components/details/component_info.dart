7d11192a41208b06b84fe16b92482ae35287cf68
rkrawczyk98 <47090131+rkrawczyk98@users.noreply.github.com>
2024-06-26 22:21:52 +0200

    Adding files missing in previous commit

---
+++ lib/src/views/widgets/components/details/component_info.dart
@@ -0,0 +1,280 @@
+import 'package:flutter/material.dart';
+import 'package:quality_control_mobile/src/utils/formatters/date_formater.dart';
+
+class ComponentInfo extends StatelessWidget {
+  final String name;
+  final DateTime? controlDate;
+  final DateTime? productionDate;
+  final double size;
+  final DateTime creationDate;
+  final DateTime lastModified;
+  final DateTime? deletedAt;
+  final String createdByUser;
+  final String modifiedByUser;
+  final String componentType;
+  final String status;
+  final String delivery;
+  final DateTime? deliveryDate;
+  final String warehouse;
+  final String warehousePosition;
+  final DateTime? scrappedAt;
+
+  const ComponentInfo({
+    super.key,
+    required this.name,
+    this.controlDate,
+    this.productionDate,
+    required this.size,
+    required this.creationDate,
+    required this.lastModified,
+    this.deletedAt,
+    required this.createdByUser,
+    required this.modifiedByUser,
+    required this.componentType,
+    required this.status,
+    required this.delivery,
+    required this.deliveryDate,
+    required this.warehouse,
+    required this.warehousePosition,
+    this.scrappedAt,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.grey[850],
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                const Text(
+                  'Nazwa',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  name,
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+              Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                const Text(
+                  'Typ',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  componentType,
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+            ]),
+            const SizedBox(height: 16),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                const Text(
+                  'Numer dostawy',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  delivery, ////////////////////////////////////////////////////////////////////////
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+              Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                const Text(
+                  'Data dostawy',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  formatDate(deliveryDate),
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+            ]),
+            const SizedBox(height: 16),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                const Text(
+                  'Magazyn',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  warehouse,
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+              Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                const Text(
+                  'Pozycja magazynowa',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  warehousePosition,
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+            ]),
+            const SizedBox(height: 16),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                const Text(
+                  'Status',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  status,
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+              Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                const Text(
+                  'Data kontroli',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  formatDate(controlDate),
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+            ]),
+            const SizedBox(height: 16),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                const Text(
+                  'Rozmiar',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  size.toString(),
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+              Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                const Text(
+                  'Data produkcji',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  formatDate(productionDate),
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+            ]),
+            const SizedBox(height: 16),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Flexible(
+                flex: 1, // Połowa szerokości dla każdej kolumny
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Autor ostatnich zmian',
+                      style: TextStyle(color: Colors.grey),
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      modifiedByUser,
+                      style: const TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                      softWrap: true, // Zawijanie tekstu
+                    ),
+                  ],
+                ),
+              ),
+              // const SizedBox(height: 4, width: 16,),
+              Flexible(
+                flex: 1, // Połowa szerokości dla każdej kolumny
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text(
+                      'Data ostatniej modyfikacji',
+                      style: TextStyle(
+                          color: Colors.grey, overflow: TextOverflow.ellipsis),
+                      maxLines: 2,
+                      textAlign: TextAlign.right,
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      formatDate(lastModified),
+                      style: const TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                      softWrap: true, // Zawijanie tekstu
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+            const SizedBox(height: 16),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                const Text(
+                  'Dodany przez',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  createdByUser,
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+              Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+                const Text(
+                  'Data dodania',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  formatDate(creationDate),
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+            ]),
+            const SizedBox(height: 16),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                const Text(
+                  'Data złomowania',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  formatDate(scrappedAt),
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ]),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/views/widgets/components/details/subcomponent_list.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quality_control_mobile/src/data/providers/component_subcomponent_provider.dart';
+import 'package:quality_control_mobile/src/data/providers/subcomponent_provider.dart';
+import 'package:quality_control_mobile/src/data/providers/subcomponent_status_provider.dart';
+
+class SubcomponentsList extends StatelessWidget {
+  final int componentId;
+
+  const SubcomponentsList({super.key, required this.componentId});
+
+  @override
+  Widget build(BuildContext context) {
+    var subcomponentProvider = Provider.of<SubcomponentProvider>(context, listen: false);
+    var subcomponentStatusProvider = Provider.of<SubcomponentStatusProvider>(context, listen: false);
+
+    return Consumer<ComponentSubcomponentProvider>(
+      builder: (context, provider, child) {
+        var subcomponents = provider.componentsubcomponents.where((c) => c.componentId == componentId).toList();
+
+        return ListView.builder(
+          shrinkWrap: true,
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: subcomponents.length,
+          itemBuilder: (context, index) {
+            var subcomponent = subcomponents[index];
+            var subcomponentDetails = subcomponentProvider.subcomponents.firstWhere(
+              (s) => s.id == subcomponent.subcomponentId,
+              orElse: () => throw Exception('Subcomponent not found'),
+            );
+
+            return ListTile(
+              leading: const Icon(Icons.extension),
+              title: Text(subcomponentDetails.name),
+              subtitle: DropdownButton<int>(
+                value: subcomponent.statusId,
+                onChanged: (newValue) {
+                  if (newValue != null) {
+                    provider.updateSubcomponentStatus(subcomponent.subcomponentId, newValue);
+                  }
+                },
+                items: subcomponentStatusProvider.statuses.map((status) {
+                  return DropdownMenuItem<int>(
+                    value: status.id,
+                    child: Text(status.name),
+                  );
+                }).toList(),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
